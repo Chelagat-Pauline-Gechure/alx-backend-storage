12740e9501bd3326157ebebe49e1dfdce3f573ec
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2024-01-18 08:50:48 +0000

    1. In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- A SQL script that creates a table (called users) with following fields
+-- id, email, name, country
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+)
