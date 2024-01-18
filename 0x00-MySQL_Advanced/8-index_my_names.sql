39c4c75c4771925d5002e48ad46a136f9c03b3dd
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2024-01-18 18:24:36 +0000

    8. Optimize simple search

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,5 +1,5 @@
 -- SQL script that creates an index idx_name_first
 -- on the table names and the first letter of name.
-- Only the first letter of name must be indexed
+-- Only the first letter of name must be indexed
 
-CREATE INDEX idx_name_first ON names(name(1))
+CREATE INDEX idx_name_first ON names(name(1));
